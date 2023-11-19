48fe395aea7ad4264aa066933c54a3137f97e4c4
moreum2 <jeon4567@naver.com>
2023-11-20 04:10:48 +0900

    구글맵 gardle 추가

--- app/src/main/java/com/example/potholego/SubActivity.kt
+++ app/src/main/java/com/example/potholego/SubActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.view.View
 
 class SubActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sub)
