ebc5f907c80f0715d87927c76758eb173df1c3be
moreum2 <jeon4567@naver.com>
2023-11-21 04:08:51 +0900

    Revert "Revert "사진 저장하기, 뒤로가기""
    This reverts commit 4e94208e521146eacec3a6676fbb1c3cc9454c6a.

--- app/src/main/java/com/example/potholego/MainActivity.kt
+++ app/src/main/java/com/example/potholego/MainActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import android.content.Intent
-import androidx.appcompat.widget.Toolbar
 import android.widget.Switch
 
 class MainActivity : AppCompatActivity() {
--- app/src/main/java/com/example/potholego/SubActivity.kt
+++ app/src/main/java/com/example/potholego/SubActivity.kt
@@ -2,13 +2,25 @@
 package com.example.potholego
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import android.view.View
-import androidx.fragment.app.FragmentTransaction
 
 class SubActivity : AppCompatActivity() {
+
+    lateinit var mFragment: FullScreenImageDialogFragment
+
+    override fun onBackPressed() {
+        if (!mFragment.isHidden) {
+            var transaction = supportFragmentManager.beginTransaction()
+            transaction.hide(mFragment)
+            transaction.commit()
+        } else {
+            super.onBackPressed()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,24 +46,21 @@ class SubActivity : AppCompatActivity() {
 
         // ImageView 클릭 이벤트 처리
         imgProfile.setOnClickListener {
-            // 전체화면 이미지를 표시하는 Fragment로 전환
-            val fullScreenFragment = FullScreenImageDialogFragment().apply {
-                arguments = Bundle().apply {
-                    // 이미지 리소스 ID를 전달
-                    putInt(FullScreenImageDialogFragment.ARG_IMAGE_RES_ID, data?.img ?: 0)
-                }
-            }
+            mFragment.setImageResource(FullScreenImageDialogFragment.ARG_IMAGE_RES_ID, data?.img ?: 0)
 
             // Fragment를 표시하기 위해 FragmentTransaction을 사용
-            supportFragmentManager.beginTransaction().apply {
-                replace(R.id.dummyFullScreenView, fullScreenFragment)
-                addToBackStack(null)
-                setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-                commit()
-            }
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.dummyFullScreenView, mFragment)
 
-            // dummyFullScreenView 화면에 보이게 설정
-            findViewById<View>(R.id.dummyFullScreenView).setVisibility(View.VISIBLE)
+            // dummyFullScreenView를 숨기고, fragment_full_screen_image를 보이게 설정
+            transaction.show(mFragment)
+
+            // 트랜잭션을 커밋
+            transaction.commit()
         }
+        mFragment = supportFragmentManager.findFragmentById(R.id.dummyFullScreenView) as FullScreenImageDialogFragment
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.hide(mFragment)
+        transaction.commit()
     }
 }
