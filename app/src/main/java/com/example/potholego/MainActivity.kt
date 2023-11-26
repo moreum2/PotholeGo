0f508a5cba9140aa03608d7f86e336eb0d7d27e2
moreum2 <jeon4567@naver.com>
2023-11-27 02:26:52 +0900

    코드 단순화

--- app/src/main/java/com/example/potholego/MainActivity.kt
+++ app/src/main/java/com/example/potholego/MainActivity.kt
@@ -39,9 +39,10 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val pathReference = storageRef.child("images").listAll().addOnSuccessListener {
-            it.items.forEach {
-                Log.e("File name: ", it.name)
+        lifecycleScope.launch {
+            val list = storageRef.child("images").listAll().await()
+            list.items.map {
+//                ProfileData(imageUrl = it.downloadUrl.await())
             }
         }
         setContentView(R.layout.activity_main)
@@ -51,7 +52,6 @@ class MainActivity : AppCompatActivity() {
 
         val switchButton: Switch = findViewById(R.id.switchButton)
         switchButton.setOnCheckedChangeListener { _, isChecked ->
-            updateData(isChecked)
         }
     }
 
@@ -72,57 +72,7 @@ class MainActivity : AppCompatActivity() {
         })
         val rv_profile = findViewById<RecyclerView>(R.id.rv_profile)
         rv_profile.adapter = profileAdapter
-
-        // 초기 데이터 설정
-        updateData(false)
     }
 
-    private fun updateData(showAll: Boolean) {
-        datas.clear()
 
-        // Coroutine을 사용하여 백그라운드 스레드에서 데이터 가져오기
-        lifecycleScope.launch(Dispatchers.IO) {
-            try {
-                val querySnapshot = firestore.collection("images").get().await()
-                for (document in querySnapshot.documents) {
-                    val name = document.getString("name") ?: ""
-                    val date = document.getString("date") ?: ""
-                    val vibrationDetected = document.getBoolean("vibrationDetected") ?: false
-                    val institution = document.getString("institution") ?: ""
-                    val imageUrl = document.getString("imageUrl") ?: ""
-
-                    // Firebase Storage에서 이미지 다운로드
-                    val storageRef: StorageReference = storage.reference.child(imageUrl)
-                    val uri = storageRef.downloadUrl.await()
-
-                    withContext(Dispatchers.Main) {
-                        datas.add(
-                            ProfileData(
-                                imgUrl = uri.toString(),
-                                name = name,
-                                date = date,
-                                vibrationDetected = vibrationDetected,
-                                institution = institution
-                            )
-                        )
-
-
-                        // TextFileProcessor를 사용하여 텍스트 파일을 처리
-                        val textFileProcessor = TextFileProcessor(storage)
-                        textFileProcessor.processTextFile(
-                            imageUrl,
-                            onSuccess = { textFileUrl ->
-                                // 텍스트 파일을 성공적으로 처리한 경우의 작업
-                                Log.d("MainActivity", "Successfully processed text file: $textFileUrl")
-                            },
-                            onFailure = {
-                                // 텍스트 파일 처리 실패 시의 작업
-                                Log.e("MainActivity", "Failed to process text file")
-                            }
-                        )
-                    }
-                }
-            } catch (exception: Exception){}
-        }
-    }
 }
